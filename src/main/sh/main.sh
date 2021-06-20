763a327eeb9848e520b2933b5a46d010b687584f
sh-pm <shpmmail@gmail.com>
2021-06-20 15:40:00 -0300

    v0.1.0
    Initial commit and push

--- src/main/sh/main.sh
+++ src/main/sh/main.sh
@@ -9,7 +9,7 @@ include_file "$SRC_DIR_PATH"/functions/functions.sh
 log_info "Test show some informative log text" # log_info is a reusable function inside sh-logger lib
 log_warn "Test show some warning log text" # log_warn is a reusable function inside sh-logger lib
 
-ensure_number_params_correct 1 $@
+ensure_number_params_correct 1 "$@"
 
 logstring "$1"
 
